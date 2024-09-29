7cc45d3668f556654b78a7766706b723e7324cd0
anudevSaraswat <anudev@tech-x.com>
2024-09-29 17:36:21 +0530

    - changed shorten API's method type to POST - added cors headers

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/anudevSaraswat/cutshort-url/handlers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,7 +10,13 @@ func SetupRoutes() *gin.Engine {
 
 	server := gin.Default()
 
-	server.GET("/api/shorten", handlers.APIShortenURL)
+	server.Use(cors.New(cors.Config{
+		AllowMethods: []string{"OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		AllowHeaders: []string{"Origin", "Content-Length", "Authorization", "Content-Type", "Options", "Accept", "Referer", "User-Agent", "Version"},
+		AllowOrigins: []string{"*"},
+	}))
+
+	server.POST("/api/shorten", handlers.APIShortenURL)
 	server.GET("/api/resolve/:short_url", handlers.APIResolveURL)
 
 	return server
